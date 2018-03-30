cacef8e031624af8e3021fefa46848930c595953
Sarthak Sahu <vishalsahu5@gmail.com>
2018-03-31 00:33:28 +0530

    information is now being displayed. user gets to select option

--- music.py
+++ music.py
@@ -13,7 +13,8 @@ def print_all_links(search_results):
 			None
 	"""
 	for link in search_results:
-		print("http://www.youtube.com/watch?v=" + link)
+		# print("http://www.youtube.com/watch?v=" + link)
+		print(link)
 
 def get_links(song_name):
 	"""
@@ -30,32 +31,39 @@ def get_links(song_name):
 	html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
 
 	search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+
+	youtube_string = "http://www.youtube.com/watch?v=";
+	for i in range(0,len(search_results)):
+		search_results[i] = youtube_string + search_results[i]
 	return search_results
 
 
 def display_information(search_results):
 	"""
+		Prints the details of the video.
 		@param:
 			A list of links
 		returns:
 			None
 	"""
 	ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
-	with ydl:
-		result = ydl.extract_info(
-			search_results,
-			download=False # We just want to extract the info
-		)
-	if 'entries' in result:
-	    # Can be a playlist or a list of videos
-	    video = result['entries'][0]
-	else:
-	    # Just a video
-	    video = result
+	for i in range(0, len(search_results)):
+		with ydl:
+			result = ydl.extract_info(
+				search_results[i],
+				download=False # We just want to extract the info
+			)
+		if 'entries' in result:
+		    # Can be a playlist or a list of videos
+		    video = result['entries'][0]
+		else:
+		    # Just a video
+		    video = result
 
-	print(video)
-	video_url = video['url']
-	print(video_url)	
+		# print(video)
+		print(video['title'])
+		# video_url = video['webpage_url']
+		# print(video_url)	
 
 
 def query_and_download():
@@ -69,15 +77,16 @@ def query_and_download():
 	song_name = input("Enter a song name: ").strip()
 	
 	search_results = get_links(song_name)
+	display_information(search_results)
+	# print_all_links(search_results)
 
-	# display_information(search_results)
-	print_all_links(search_results)
+	num = input("which one would you like to download ? : ").strip()
 	
-	print("{} results found. Downloading the first one.".format(len(search_results)))
+	print("{} results found. Downloading song number {}.".format(len(search_results), int(num)))
 	ydl_opts = {'preferredcodec':'mp3'}
 	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 		link = []
-		link.append(search_results[0])
+		link.append(search_results[int(num)-1])
 		ydl.download(link)
 
 
