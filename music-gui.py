741af2bfc5334b789cb6801212e8a734ecd0eab2
Sarthak Sahu <vishalsahu5@gmail.com>
2018-03-31 05:02:40 +0530

    cosmetic changes

--- music-gui.py
+++ music-gui.py
@@ -1,3 +1,7 @@
+#############################################################
+# Imports
+#############################################################
+
 from __future__ import unicode_literals
 import os
 
@@ -14,9 +18,109 @@ import sys
 import urllib.request
 import urllib.parse
 import re
-quality="192"
 
+#############################################################
+# Global Variables
+#############################################################
+
+quality="192" 
 search_results = []
+
+#############################################################
+# Utility functions
+#############################################################
+
+def print_all_links(search_results):
+    """
+        @param:
+            search_results: a list of links
+        returns
+            None
+    """
+    for link in search_results:
+        # print("http://www.youtube.com/watch?v=" + link)
+        print(link)
+
+
+def get_links(song_name):
+    """
+        @param
+            song_name: a string
+        returns
+            A list of youtube links
+    """
+
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+
+    print("searching...")
+    query_string = urllib.parse.urlencode({"search_query": song_name})
+    html_content = urllib.request.urlopen(
+        "http://www.youtube.com/results?" + query_string)
+
+    search_results = re.findall(
+        r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+
+    youtube_string = "http://www.youtube.com/watch?v="
+    for i in range(0, len(search_results)):
+        search_results[i] = youtube_string + search_results[i]
+    search_results = list(set(search_results))
+    return search_results
+
+
+def display_information(search_results):
+    """
+        Prints the details of the video.
+        @param:
+            A list of links
+        returns:
+            titles of videos as a list
+    """
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+    video_titles = []
+    for i in range(0, min(len(search_results),10)):
+        try:
+            with ydl:
+                result = ydl.extract_info(
+                    search_results[i],
+                    download=False  # We just want to extract the info
+                )
+            if 'entries' in result:
+                # Can be a playlist or a list of videos
+                video = result['entries'][0]
+            else:
+                # Just a video
+                video = result
+            # print(video)
+            video_titles.append(video['title'])
+            # video_url = video['webpage_url']
+            # print(video_url)
+        except Exception as e:
+            continue
+    for i in range(0, len(video_titles)):
+        print(str(i+1) + " -> " + video_titles[i])
+    return video_titles
+
+
+def query_and_download(text):
+    """
+        @param
+            text: Receive a string from user from gui
+        returns
+            None
+    """
+    global search_results
+    song_name = text.strip()
+
+    search_results = get_links(song_name)
+    video_titles = display_information(search_results)
+    show_titles(video_titles)
+
+
+#############################################################
+# GUI Functionality
+#############################################################
+
+
 class filterit:
     global quality
     value_of_combo = '192'
@@ -96,111 +200,10 @@ def add_placeholder_to(entry, placeholder, color="grey", font=None):
 
     return state
 
-#  ######downloading module############################################
-
-
-def print_all_links(search_results):
-	"""
-		@param:
-			search_results: a list of links
-		returns
-			None
-	"""
-	for link in search_results:
-		# print("http://www.youtube.com/watch?v=" + link)
-		print(link)
-
-
-def get_links(song_name):
-    """
-        @param
-            song_name: a string
-        returns
-            A list of youtube links
-    """
-
-    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
-
-    print("searching...")
-    query_string = urllib.parse.urlencode({"search_query": song_name})
-    html_content = urllib.request.urlopen(
-        "http://www.youtube.com/results?" + query_string)
-
-    search_results = re.findall(
-        r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-
-    youtube_string = "http://www.youtube.com/watch?v="
-    for i in range(0, len(search_results)):
-        search_results[i] = youtube_string + search_results[i]
-    search_results = list(set(search_results))
-    return search_results
-
-
-def display_information(search_results):
-    """
-        Prints the details of the video.
-        @param:
-            A list of links
-        returns:
-            titles of videos as a list
-    """
-    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
-    video_titles = []
-    for i in range(0, min(len(search_results),10)):
-        try:
-            with ydl:
-                result = ydl.extract_info(
-                    search_results[i],
-                    download=False  # We just want to extract the info
-                )
-            if 'entries' in result:
-                # Can be a playlist or a list of videos
-                video = result['entries'][0]
-            else:
-                # Just a video
-                video = result
-            # print(video)
-            video_titles.append(video['title'])
-            # video_url = video['webpage_url']
-            # print(video_url)
-        except Exception as e:
-            continue
-    for i in range(0, len(video_titles)):
-        print(str(i+1) + " -> " + video_titles[i])
-    return video_titles
-
-
-def query_and_download(text):
-    """
-        @param
-            text: Receive a string from user from gui
-        returns
-            None
-    """
-    global search_results
-    song_name = text.strip()
-
-    search_results = get_links(song_name)
-    video_titles = display_information(search_results)
-    show_titles(video_titles)
-    # print_all_links(search_results)
-
-    # num = input("which one would you like to download ? : ").strip()
-    # num = 
-
-    # print("{} results found. Downloading song number {}.".format(len(search_results), int(num)))
-    # ydl_opts = {'preferredcodec': 'mp3'}
-    # with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    #     link = []
-    #     link.append(search_results[int(num)-1])
-    #     ydl.download(link)
-
-
-
-# #######################################################################################################
 class SearchBox(Frame):
-    """
-               this class is  search box changes colour when we write and other functionality to look like a search box
+        """
+            This class is  search box changes colour when we write and other 
+            functionality to look like a search box
         """
     def __init__(self, master, entry_width=30, entry_font=None, entry_background="white", entry_highlightthickness=1, button_text="Search", button_ipadx=10, button_background="#009688", button_foreground="white", button_font=None, opacity=0.8, placeholder=None, placeholder_font=None, placeholder_color="grey", spacing=3, command=None):
         Frame.__init__(self, master)
@@ -282,15 +285,17 @@ class SearchBox(Frame):
 
     def _state_active(self, event):
         self.button_label.configure(background=self._button_activebackground)
-# #################search box ends##################
+###################search box ends##################
+
+
 def show_titles(video_titles):
     video_titles.reverse()
     for items in video_titles:
         listbox.insert(0,items)
 
-
 def Downloadit(event):
     global search_results
+    global quality
     index = listbox.get(0, "end").index(listbox.get('active'))
     print(index)
     print("{} results found. Downloading song number {}.".format(len(search_results), int(index)))
@@ -302,6 +307,7 @@ def Downloadit(event):
             'preferredquality':quality,
         }],
     }
+    print("quality: " + str(quality))
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         link = []
         link.append(search_results[int(index)])
