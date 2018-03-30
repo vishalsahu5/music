05c6b1a8b9e724833f4639c9c286a9b5b9f1c3d3
Yash Jain <yjain271196@gmail.com>
2018-03-31 03:45:45 +0530

    Download selected song from list

--- music-gui.py
+++ music-gui.py
@@ -14,6 +14,8 @@ import sys
 import urllib.request
 import urllib.parse
 import re
+
+search_results = []
 class filterit:
 
     value_of_combo = 'Songs'
@@ -109,85 +111,88 @@ def print_all_links(search_results):
 
 
 def get_links(song_name):
-	"""
-		@param
-			song_name: a string
-		returns
-			A list of youtube links
-	"""
+    """
+        @param
+            song_name: a string
+        returns
+            A list of youtube links
+    """
 
-	ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
 
-	print("searching...")
-	query_string = urllib.parse.urlencode({"search_query": song_name})
-	html_content = urllib.request.urlopen(
-	    "http://www.youtube.com/results?" + query_string)
+    print("searching...")
+    query_string = urllib.parse.urlencode({"search_query": song_name})
+    html_content = urllib.request.urlopen(
+        "http://www.youtube.com/results?" + query_string)
 
-	search_results = re.findall(
-	    r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+    search_results = re.findall(
+        r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
 
-	youtube_string = "http://www.youtube.com/watch?v="
-	for i in range(0, len(search_results)):
-		search_results[i] = youtube_string + search_results[i]
-	return search_results
+    youtube_string = "http://www.youtube.com/watch?v="
+    for i in range(0, len(search_results)):
+        search_results[i] = youtube_string + search_results[i]
+    search_results = list(set(search_results))
+    return search_results
 
 
 def display_information(search_results):
-	"""
-		Prints the details of the video.
-		@param:
-			A list of links
-		returns:
-			None
-	"""
-	ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
-	video_titles = []
-	for i in range(0, len(search_results)):
-		try:
-			with ydl:
-				result = ydl.extract_info(
-					search_results[i],
-					download=False  # We just want to extract the info
-				)
-			if 'entries' in result:
-			    # Can be a playlist or a list of videos
-			    video = result['entries'][0]
-			else:
-			    # Just a video
-			    video = result
-			# print(video)
-			video_titles.append(video['title'])
-			# video_url = video['webpage_url']
-			# print(video_url)
-		except Exception as e:
-			continue
-	for i in range(0, len(video_titles)):
-		print(str(i+1) + " -> " + video_titles[i])
+    """
+        Prints the details of the video.
+        @param:
+            A list of links
+        returns:
+            titles of videos as a list
+    """
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+    video_titles = []
+    for i in range(0, min(len(search_results),10)):
+        try:
+            with ydl:
+                result = ydl.extract_info(
+                    search_results[i],
+                    download=False  # We just want to extract the info
+                )
+            if 'entries' in result:
+                # Can be a playlist or a list of videos
+                video = result['entries'][0]
+            else:
+                # Just a video
+                video = result
+            # print(video)
+            video_titles.append(video['title'])
+            # video_url = video['webpage_url']
+            # print(video_url)
+        except Exception as e:
+            continue
+    for i in range(0, len(video_titles)):
+        print(str(i+1) + " -> " + video_titles[i])
+    return video_titles
 
 
 def query_and_download(text):
-	"""
-		@param
-			text: Receive a string from user from gui
-		returns
-			None
-	"""
+    """
+        @param
+            text: Receive a string from user from gui
+        returns
+            None
+    """
+    global search_results
+    song_name = text.strip()
 
-	song_name = text.strip()
+    search_results = get_links(song_name)
+    video_titles = display_information(search_results)
+    show_titles(video_titles)
+    # print_all_links(search_results)
 
-	search_results = get_links(song_name)
-	display_information(search_results)
-	# print_all_links(search_results)
+    # num = input("which one would you like to download ? : ").strip()
+    # num = 
 
-	num = input("which one would you like to download ? : ").strip()
-
-	print("{} results found. Downloading song number {}.".format(
-	    len(search_results), int(num)))
-	ydl_opts = {'preferredcodec': 'mp3'}
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		link = []
-		link.append(search_results[int(num)-1])
-		ydl.download(link)
+    # print("{} results found. Downloading song number {}.".format(len(search_results), int(num)))
+    # ydl_opts = {'preferredcodec': 'mp3'}
+    # with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    #     link = []
+    #     link.append(search_results[int(num)-1])
+    #     ydl.download(link)
 
 
 
@@ -276,6 +281,34 @@ class SearchBox(Frame):
 
     def _state_active(self, event):
         self.button_label.configure(background=self._button_activebackground)
+# #################search box ends##################
+def show_titles(video_titles):
+    video_titles.reverse()
+    for items in video_titles:
+        listbox.insert(0,items)
+
+
+def Downloadit(event):
+    global search_results
+    index = listbox.get(0, "end").index(listbox.get('active'))
+    print(index)
+    print("{} results found. Downloading song number {}.".format(len(search_results), int(index)))
+    ydl_opts={
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        link = []
+        link.append(search_results[int(index)])
+        ydl.download(link)
+
+    return index
+
+
 if __name__ == "__main__":
     from  tkinter.messagebox import showinfo
     from tkinter import *
@@ -290,4 +323,7 @@ if __name__ == "__main__":
     Filterit = filterit(root)
     listbox = Listbox(root, width=50, height=600)
     listbox.pack(side="right", padx=5)
+    Downloadbutton = Button(root, text='Download')
+    Downloadbutton.pack()
+    Downloadbutton.bind("<Button-1>", Downloadit)
     root.mainloop()
